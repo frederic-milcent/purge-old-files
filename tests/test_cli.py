3ef247caedf50b085d686871dfd721a7ec0edff3
Philippe Muller <philippe.muller@gmail.com>
2019-04-10 10:52:50 +0800

    Re-arrange test arguments on multiple lines
    This will make subsequent commits more readable.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -13,23 +13,40 @@ from tests.helpers import make_test_files_hierarchy
     'arguments, remaining, log_level, log_handler_class',
     (
         (
-            ['--dry-run', '10s'], ['foo/1', 'foo/2', 'foo/bar/baz'],
-            logging.INFO, logging.StreamHandler,
-        ),
-        (['-Ds', '1d'], [], logging.DEBUG, logging.handlers.SysLogHandler),
-        (['3M'], ['foo/1', 'foo/2'], logging.INFO, logging.StreamHandler),
-        (
-            ['--include', '[0-9]', '10d'], ['foo/1', 'foo/bar/baz'],
-            logging.INFO, logging.StreamHandler,
+            ['--dry-run', '10s'],
+            ['foo/1', 'foo/2', 'foo/bar/baz'],
+            logging.INFO,
+            logging.StreamHandler,
         ),
         (
-            ['--exclude', '[0-9]', '10d'], ['foo/1', 'foo/2'],
-            logging.INFO, logging.StreamHandler,
+            ['-Ds', '1d'],
+            [],
+            logging.DEBUG,
+            logging.handlers.SysLogHandler,
+        ),
+        (
+            ['3M'],
+            ['foo/1', 'foo/2'],
+            logging.INFO,
+            logging.StreamHandler,
+        ),
+        (
+            ['--include', '[0-9]', '10d'],
+            ['foo/1', 'foo/bar/baz'],
+            logging.INFO,
+            logging.StreamHandler,
+        ),
+        (
+            ['--exclude', '[0-9]', '10d'],
+            ['foo/1', 'foo/2'],
+            logging.INFO,
+            logging.StreamHandler,
         ),
         (
             ['--include', '[0-9]', '--exclude', '2', '-q', '10d'],
             ['foo/1', 'foo/2', 'foo/bar/baz'],
-            logging.WARNING, logging.StreamHandler
+            logging.WARNING,
+            logging.StreamHandler
         ),
     ),
 )
